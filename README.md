cdca67da0c199c2dee9532c6d74a820a8116760e
Remko Tronçon <git@el-tramo.be>
2015-11-21 17:36:20 +0100

    Import

---
+++ README.md
@@ -0,0 +1,13 @@
+# [Diceware & Memorable Random-letter Passphrase generator](https://el-tramo.be/password-generator/)
+
+Gnerates passwords using 2 methods described by [Arnold G.
+Reinhold](http://world.std.com/%7Ereinhold/):
+[Diceware](http://world.std.com/~reinhold/diceware.html) and [Memorable
+Random-letter
+Passphrases](http://diceware.blogspot.be/2013/12/making-random-letter-passwords-memorable.html).
+The implementation relies on [random.js](https://github.com/ckknight/random-js)
+for generating uniformly distributed random values using the browsers'
+generated cryptographically random numbers.  All the code runs in the
+browser, nothing is sent to the server.
+
+You can see the code in action [here](https://el-tramo.be/password-generator/).
---
+++ index.js
@@ -0,0 +1,208 @@
+/* global $, _, Random */
+
+$(function () {
+	var random = new Random(Random.browserCrypto);
+
+	function getColorForEntropy(entropy) {
+		return entropy < 50 ? "red"
+			: entropy > 80 ? "green"
+			: "orange";
+	}
+
+	////////////////////////////////////////////////////////////////////////////////
+	// Diceware
+	////////////////////////////////////////////////////////////////////////////////
+	
+	(function() {
+		var lists = [
+			{ name: "English", url: "/password-generator/lists/diceware/diceware.wordlist.asc", source: "http://world.std.com/%7Ereinhold/diceware.wordlist.asc" },
+			{ name: "English (Beale)", url: "/password-generator/lists/diceware/beale.wordlist.asc", source: "http://world.std.com/%7Ereinhold/beale.wordlist.asc" },
+			{ name: "Dutch (Bart Van den Eynde)", url: "/password-generator/lists/diceware/DicewareDutch.txt", source: "http://theworld.com/%7Ereinhold/DicewareDutch.txt" }
+		];
+		var currentList;
+		var currentListSource;
+
+		function setControlsEnabled(enabled) {
+			$("#diceware-lists").prop("disabled", !enabled);
+			$("#diceware-count").prop("disabled", !enabled);
+			$("#diceware").toggleClass("disabled", !enabled);
+		}
+
+		function getDicewareWords(list, count) {
+			return _.times(count, function () {
+				return list[random.integer(0, list.length - 1)];
+			}).join(" ");
+		}
+
+		function update() {
+			if (currentList) {
+				$("#diceware-list-count").text("(" + currentList.length + " words)");
+				$("#diceware-list-source").attr("href", currentListSource);
+				var count = parseInt($("#diceware-count").val(), 10);
+
+				$("#diceware-result")
+					.val(getDicewareWords(currentList, count))
+					.height(0)
+					.height($("#diceware-result").prop('scrollHeight') - 10);
+
+				var entropy = Math.floor(count*Math.log(currentList.length) / Math.log(2));
+				$("#diceware-entropy")
+					.text("(" + entropy + " bits entropy)")
+					.css("color", getColorForEntropy(entropy));
+			}
+		}
+
+		function loadList() {
+			currentList = undefined;
+			currentListSource = undefined;
+			setControlsEnabled(false);
+			var list = lists[$("#diceware-lists").val()];
+			$.ajax(list.url)
+				.then(function (result) {
+					var lines = result.split("\n");
+					var newList = [];
+					for (var i = 0; i < lines.length; ++i) {
+						var m;
+						if (m = lines[i].match(/\d\d\d\d\d\s+(.*)/)) { // eslint-disable-line no-cond-assign
+							newList.push(m[1]);
+						}
+					}
+					currentList = newList;
+					currentListSource = list.source;
+				})
+				.fail(function (req) {
+					console.log(req);
+				})
+				.always(function () {
+					setControlsEnabled(true);
+					update();
+				});
+		}
+
+		$("#diceware-lists").append(lists.map(function (val, i) {
+			return $("<option />").val(i).text(val.name);
+		}));
+
+		$("#diceware-count").on("change", update);
+		$("#diceware-refresh").on("click", update);
+		$("#diceware-lists").on("change", loadList);
+
+		loadList();
+	})();
+
+
+	////////////////////////////////////////////////////////////////////////////////
+	// Random-letter Passphrases
+	////////////////////////////////////////////////////////////////////////////////
+	
+	(function () {
+		var MAX_MNEMONIC_SENTENCES = 20;
+
+		var lists = [
+			{ name: "English (Reinhold)", url: "/password-generator/lists/passphrases/reinhold.json" }
+		];
+		var currentList;
+
+		function setControlsEnabled(enabled) {
+			$("#passphrase-lists").prop("disabled", !enabled);
+			$("#passphrase-count").prop("disabled", !enabled);
+			$("#passphrase").toggleClass("disabled", !enabled);
+		}
+		
+		var partition = _.memoize(function (n, max) {
+			var result = [];
+			for (var i = Math.min(max, n); i >= 2; --i) {
+				if (n - i === 0) {
+					result.push([i]);
+				}
+				else {
+					var p = partition(n - i, max).map(function (partition) {
+						return [i].concat(partition);
+					});
+					result = result.concat(p);
+				}
+			}
+			return result;
+		});
+
+		function createSentence(sentenceIndices, wordIndices, list) {
+			var sentence = sentenceIndices.map(function (sentenceIndex, i) {
+				var word = list[sentenceIndex][wordIndices[i]];
+				return (i ? word : _.capitalize(word)).replace(new RegExp(
+					"(" + String.fromCharCode(97 + wordIndices[i]) 
+					+ "|" + String.fromCharCode(65 + wordIndices[i]) + ")" ), "<b>$1</b>");
+			});
+			return sentence.join(" ") + (sentenceIndices.length > 1 ? "." : "!");
+		}
+
+		function update() {
+			if (currentList) {
+				$("#passphrase-list-source").attr("href", currentList.source);
+
+				var count = parseInt($("#passphrase-count").val(), 10);
+				var result = [];
+				for(var i = 0; i < count; ++i) {
+					result.push(random.integer(0, 25));
+				}
+
+				$("#passphrase-result").val(result.map(function (c) {
+					return String.fromCharCode(97 + c);
+				}).join(""));
+
+				var entropy = Math.floor(count*Math.log(26) / Math.log(2));
+				$("#passphrase-entropy")
+					.text("(" + entropy + " bits entropy)")
+					.css("color", getColorForEntropy(entropy));
+
+				var partitions = result.length === 1 ? [[1]]
+					: _(partition(result.length, currentList.words.length))
+							.sortBy("length")
+							.slice(0, MAX_MNEMONIC_SENTENCES)
+							.value();
+
+				var mnemonics = partitions.map(function (partition) {
+					var resultStart = 0;
+					var sentences = partition.map(function (sentenceLength) {
+						var sentence = createSentence(
+							currentList.sentences[sentenceLength-1],
+							result.slice(resultStart, resultStart + sentenceLength),
+							currentList.words);
+						resultStart += sentenceLength;
+						return sentence;
+					});
+					return sentences.join(" ");
+				});
+				$("#passphrase-mnemonics").empty().append(mnemonics.map(function (mnemonic) { 
+					return $("<li>" + mnemonic + "</li>");
+				}));
+			}
+		}
+
+		function loadList() {
+			currentList = undefined;
+			setControlsEnabled(false);
+			var list = lists[$("#passphrase-lists").val()];
+			$.ajax(list.url)
+				.then(function (result) {
+					currentList = result;
+				})
+				.fail(function (req) {
+					console.log(req);
+				})
+				.always(function () {
+					setControlsEnabled(true);
+					update();
+				});
+		}
+
+		$("#passphrase-lists").append(lists.map(function (val, i) {
+			return $("<option />").val(i).text(val.name);
+		}));
+
+		$("#passphrase-count").on("change", update);
+		$("#passphrase-refresh").on("click", update);
+
+		loadList();
+	})();
+});
+
